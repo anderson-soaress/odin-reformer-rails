6748091334209307a75d895e3afe9b072ae51b94
Anderson Junior <andersonsoares.jn@gmail.com>
2024-05-17 13:25:28 -0300

    feat: Add PATCH method for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show 
+    @user = User.find(params[:id])
+  end
+
   def new 
     @user = User.new
   end
@@ -14,6 +18,20 @@ class UsersController < ApplicationController
       redirect_to users_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update 
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
